9bbfbc3c30c0ac5cb3359db2d4dc5c0bc3f4ea5b
CyberTapir <callumtbennett@gmail.com>
2023-04-03 09:45:25 +1000

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim arrNum(13) As Integer
+        Dim Total As Integer
+        Total = 0
+        arrNum = {0, 5, 1, 8, 5, 4, 76, 2, 8, 2, 9, 34, 67, 278}
+        For i = 1 To 13
+            Total = Total + arrNum(i)
+        Next i
+        MsgBox(Total)
+    End Sub
+End Class
